c110584824913a7125b41f045423323351cb7417
OliverSchlueter <oliverhd.1303@gmail.com>
2023-03-17 17:01:41 +0100

    Update paperweight to 1.5.3

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     `java-library`
-    id("io.papermc.paperweight.userdev") version "1.5.1"
+    id("io.papermc.paperweight.userdev") version "1.5.3"
     id("xyz.jpenilla.run-paper") version "2.0.1" // Adds runServer and runMojangMappedServer tasks for testing
 }
 

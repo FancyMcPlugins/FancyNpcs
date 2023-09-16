a18548491e29e6f7f3c8afa7d201b612035ee115
OliverSchlueter <OliverSchlueter@users.noreply.github.com>
2023-09-16 16:38:10 +0200

    Update version to 2.0.3

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,7 +11,7 @@ plugins {
 
 allprojects {
     group = "de.oliver"
-    version = "2.0.2.1"
+    version = "2.0.3"
     description = "Simple, lightweight and fast NPC plugin using packets"
 
     repositories {

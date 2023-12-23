80a59a2bae54a40277507568e53543b6faf9e9cc
yukisnowflake <32297717+yukisnowflake@users.noreply.github.com>
2023-12-23 23:45:23 +0500

    Update Npc.java

--- api/src/main/java/de/oliver/fancynpcs/api/Npc.java
+++ api/src/main/java/de/oliver/fancynpcs/api/Npc.java
@@ -95,11 +95,9 @@ public abstract class Npc {
         if (data.getInteractionCooldown() > 0) {
             if (lastPlayerInteraction.containsKey(player.getUniqueId())) {
                 long nextAllowedInteraction = lastPlayerInteraction.get(player.getUniqueId()) + Math.round(data.getInteractionCooldown() * 1000L);
-                if (nextAllowedInteraction > System.currentTimeMillis()) {
+                if (nextAllowedInteraction > System.currentTimeMillis() && !FancyNpcsPlugin.get().getFancyNpcConfig().isInteractionCooldownMessageDisabled()) {
                     float timeLeft = (nextAllowedInteraction - System.currentTimeMillis()) / 1000F;
                     String cooldownMessage = lang.get("on-interaction-cooldown", "time", DECIMAL_FORMAT.format(timeLeft));
-
-                    if (cooldownMessage == "none") return;
                     MessageHelper.warning(player, cooldownMessage);
                     return;
                 }

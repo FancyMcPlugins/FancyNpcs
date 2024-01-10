74773b82f852ae99dcd7fafaa60e9693bd127a07
OliverSchlueter <OliverSchlueter@users.noreply.github.com>
2024-01-10 20:12:58 +0100

    Fix version

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,7 +14,7 @@ runPaper.folia.registerTask()
 allprojects {
     group = "de.oliver"
     val buildId = System.getenv("BUILD_ID")
-    version = "2.0.4" + (if (buildId != null) ".$buildId" else "")
+    version = "2.0.7" + (if (buildId != null) ".$buildId" else "")
     description = "Simple, lightweight and fast NPC plugin using packets"
 
     repositories {

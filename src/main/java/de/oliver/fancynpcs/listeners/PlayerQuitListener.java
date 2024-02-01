90d9af8cfe26001c7d67309e5d2d0a1b3f047d2b
OliverSchlueter <OliverSchlueter@users.noreply.github.com>
2024-02-01 18:56:01 +0100

    Fix display name not showing after rejoining

--- src/main/java/de/oliver/fancynpcs/listeners/PlayerQuitListener.java
+++ src/main/java/de/oliver/fancynpcs/listeners/PlayerQuitListener.java
@@ -17,6 +17,7 @@ public class PlayerQuitListener implements Listener {
             // This allows the NpcStartLookingEvent to be called when player joins back. (Because otherwise, state would remain true and no change would be detected)
             npc.getIsVisibleForPlayer().put(event.getPlayer().getUniqueId(), false);
             npc.getIsLookingAtPlayer().put(event.getPlayer().getUniqueId(), false);
+            npc.getIsTeamCreated().put(event.getPlayer().getUniqueId(), false);
             // Calling NpcStopLookingEvent.
             Bukkit.getPluginManager().callEvent(new NpcStopLookingEvent(npc, event.getPlayer()));
         }

66618991ce888887b0a16fda601ba8d542256b06
OliverSchlueter <OliverSchlueter@users.noreply.github.com>
2024-03-05 18:01:18 +0100

    Revert "Fix right click not working on non player npcs (#91)"
    This reverts commit 9c83a8bd0b376dc6e5f214ebe03991d2335a6c3d.

--- src/main/java/de/oliver/fancynpcs/listeners/PlayerUseUnknownEntityListener.java
+++ src/main/java/de/oliver/fancynpcs/listeners/PlayerUseUnknownEntityListener.java
@@ -8,27 +8,25 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.inventory.EquipmentSlot;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.UUID;
-
 public class PlayerUseUnknownEntityListener implements Listener {
-
-    private final List<UUID> interacts = new ArrayList<>();
 
     @EventHandler
     public void onPlayerUseUnknownEntity(PlayerUseUnknownEntityEvent event) {
-        // Right click is called twice, so we need to prevent that
-        if (interacts.contains(event.getPlayer().getUniqueId())) return;
-        interacts.add(event.getPlayer().getUniqueId());
-        FancyNpcs.getInstance().getServer().getScheduler().runTaskLaterAsynchronously(FancyNpcs.getInstance(),
-                () -> interacts.remove(event.getPlayer().getUniqueId()), 5);
-
-        if (event.getHand() != EquipmentSlot.HAND) return;
-
         Npc npc = FancyNpcs.getInstance().getNpcManagerImpl().getNpc(event.getEntityId());
-        if (npc == null) return;
+        if (npc == null) {
+            return;
+        }
+
+        if (npc.getData().getType() == EntityType.VILLAGER && event.getHand() == EquipmentSlot.HAND && event.getClickedRelativePosition() == null) {
+            npc.interact(event.getPlayer());
+            return;
+        }
+
+        if (!event.isAttack() && (event.getHand() == EquipmentSlot.HAND || event.getClickedRelativePosition() != null)) {
+            return;
+        }
 
         npc.interact(event.getPlayer());
     }
+
 }

2815d261e1d0da992461a7415bd6b93abd2464cf
Michał Czopek <44530932+Grabsky@users.noreply.github.com>
2024-04-07 23:25:52 +0200

    /npc moveHere

--- src/main/java/de/oliver/fancynpcs/commands/npc/MoveHereCMD.java
+++ src/main/java/de/oliver/fancynpcs/commands/npc/MoveHereCMD.java
@@ -1,7 +1,6 @@
 package de.oliver.fancynpcs.commands.npc;
 
-import de.oliver.fancylib.LanguageConfig;
-import de.oliver.fancylib.MessageHelper;
+import de.oliver.fancylib.translations.Translator;
 import de.oliver.fancynpcs.FancyNpcs;
 import de.oliver.fancynpcs.api.Npc;
 import de.oliver.fancynpcs.api.events.NpcModifyEvent;
@@ -9,14 +8,15 @@ import de.oliver.fancynpcs.commands.Subcommand;
 import org.bukkit.Location;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 
 import java.util.List;
 
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
 public class MoveHereCMD implements Subcommand {
 
-    private final LanguageConfig lang = FancyNpcs.getInstance().getLanguageConfig();
+    private final Translator translator = FancyNpcs.getInstance().getTranslator();
 
     @Override
     public List<String> tabcompletion(@NotNull Player player, @Nullable Npc npc, @NotNull String[] args) {
@@ -24,20 +24,20 @@ public class MoveHereCMD implements Subcommand {
     }
 
     @Override
-    public boolean run(@NotNull CommandSender receiver, @Nullable Npc npc, @NotNull String[] args) {
-        if (!(receiver instanceof Player player)) {
-            MessageHelper.error(receiver, lang.get("npc-command.only-players"));
+    public boolean run(@NotNull CommandSender sender, @Nullable Npc npc, @NotNull String[] args) {
+        if (!(sender instanceof Player player)) {
+            translator.translate("command_player_only").send(sender);
             return false;
         }
 
         if (npc == null) {
-            MessageHelper.error(receiver, lang.get("npc-not-found"));
+            translator.translate("command_invalid_npc").replace("npc", args[1]).send(sender);
             return false;
         }
 
         Location location = player.getLocation();
 
-        NpcModifyEvent npcModifyEvent = new NpcModifyEvent(npc, NpcModifyEvent.NpcModification.LOCATION, location, receiver);
+        NpcModifyEvent npcModifyEvent = new NpcModifyEvent(npc, NpcModifyEvent.NpcModification.LOCATION, location, sender);
         npcModifyEvent.callEvent();
 
         String oldWorld = npc.getData().getLocation().getWorld().getName();
@@ -51,10 +51,9 @@ public class MoveHereCMD implements Subcommand {
                 npc.removeForAll();
                 npc.spawnForAll();
             }
-
-            MessageHelper.success(receiver, lang.get("npc-command-moveHere-moved", "npc", npc.getData().getName()));
+            translator.translate("npc_moveHere_success").replace("npc", npc.getData().getName()).send(sender);
         } else {
-            MessageHelper.error(receiver, lang.get("npc-command-modification-cancelled", "npc", npc.getData().getName()));
+            translator.translate("command_npc_modification_cancelled").send(sender);
         }
 
         return true;

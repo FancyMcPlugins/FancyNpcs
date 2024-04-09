0d20b8fda811b01f4d80009178ab5691091e9254
Michał Czopek <44530932+Grabsky@users.noreply.github.com>
2024-04-09 20:42:17 +0200

    /npc teleport

--- src/main/java/de/oliver/fancynpcs/commands/npc/TeleportCMD.java
+++ src/main/java/de/oliver/fancynpcs/commands/npc/TeleportCMD.java
@@ -1,7 +1,6 @@
 package de.oliver.fancynpcs.commands.npc;
 
-import de.oliver.fancylib.LanguageConfig;
-import de.oliver.fancylib.MessageHelper;
+import de.oliver.fancylib.translations.Translator;
 import de.oliver.fancynpcs.FancyNpcs;
 import de.oliver.fancynpcs.api.Npc;
 import de.oliver.fancynpcs.api.events.NpcModifyEvent;
@@ -12,16 +11,17 @@ import org.bukkit.World;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.util.RayTraceResult;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 
 import java.text.DecimalFormat;
 import java.util.List;
 
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
 public class TeleportCMD implements Subcommand {
 
-    private final LanguageConfig lang = FancyNpcs.getInstance().getLanguageConfig();
-    private final DecimalFormat DF = new DecimalFormat(".###");
+    private final Translator translator = FancyNpcs.getInstance().getTranslator();
+    private static final DecimalFormat COORDS_FORMAT = new DecimalFormat("#.##");
 
     @Override
     public List<String> tabcompletion(@NotNull Player player, @Nullable Npc npc, @NotNull String[] args) {
@@ -29,18 +29,18 @@ public class TeleportCMD implements Subcommand {
 
         if (args.length == 3) {
             return List.of(String.valueOf(rayTraceResult != null ?
-                    DF.format(rayTraceResult.getHitPosition().getX()) :
-                    DF.format(player.getLocation().getX())).replace(',', '.')
+                    COORDS_FORMAT.format(rayTraceResult.getHitPosition().getX()) :
+                    COORDS_FORMAT.format(player.getLocation().getX())).replace(',', '.')
             );
         } else if (args.length == 4) {
             return List.of(String.valueOf(rayTraceResult != null ?
-                    DF.format(rayTraceResult.getHitPosition().getY()) :
-                    DF.format(player.getLocation().getY())).replace(',', '.')
+                    COORDS_FORMAT.format(rayTraceResult.getHitPosition().getY()) :
+                    COORDS_FORMAT.format(player.getLocation().getY())).replace(',', '.')
             );
         } else if (args.length == 5) {
             return List.of(String.valueOf(rayTraceResult != null ?
-                    DF.format(rayTraceResult.getHitPosition().getZ()) :
-                    DF.format(player.getLocation().getZ())).replace(',', '.')
+                    COORDS_FORMAT.format(rayTraceResult.getHitPosition().getZ()) :
+                    COORDS_FORMAT.format(player.getLocation().getZ())).replace(',', '.')
             );
         } else if (args.length == 6) {
             return List.of(player.getLocation().getWorld().getName());
@@ -50,26 +50,37 @@ public class TeleportCMD implements Subcommand {
     }
 
     @Override
-    public boolean run(@NotNull CommandSender receiver, @Nullable Npc npc, @NotNull String[] args) {
+    public boolean run(@NotNull CommandSender sender, @Nullable Npc npc, @NotNull String[] args) {
         if (npc == null) {
-            MessageHelper.error(receiver, lang.get("npc-not-found"));
+            translator.translate("command_invalid_npc").replace("npc", args[1]).send(sender);
             return false;
         }
 
         if (args.length < 5) {
-            MessageHelper.error(receiver, lang.get("wrong-usage"));
+            translator.translate("npc_teleport_syntax").send(sender);
             return false;
         }
 
         double x, y, z;
 
-        try {
+        try { // NOTE: Additional try-catch blocks has been added to improve error message, will be improved during commands rework.
             x = Double.parseDouble(args[2]);
+        } catch (NumberFormatException e) {
+            translator.translate("command_invalid_number").replace("input", args[2]).send(sender);
+            return false;
+        }
+
+        try { // NOTE: Additional try-catch blocks has been added to improve error message, will be improved during commands rework.
             y = Double.parseDouble(args[3]);
+        } catch (NumberFormatException e) {
+            translator.translate("command_invalid_number").replace("input", args[3]).send(sender);
+            return false;
+        }
+
+        try { // NOTE: Additional try-catch blocks has been added to improve error message, will be improved during commands rework.
             z = Double.parseDouble(args[4]);
         } catch (NumberFormatException e) {
-            MessageHelper.error(receiver, "wrong-usage");
-            MessageHelper.error(receiver, "could-not-parse-number");
+            translator.translate("command_invalid_number").replace("input", args[4]).send(sender);
             return false;
         }
 
@@ -77,26 +88,32 @@ public class TeleportCMD implements Subcommand {
 
         if (args.length == 6) {
             world = Bukkit.getWorld(args[5]);
-        } else if (receiver instanceof Player p) {
+        } else if (sender instanceof Player p) {
             world = p.getWorld();
         }
 
         if (world == null) {
-            MessageHelper.error(receiver, "world-not-found");
+            translator.translate("command_invalid_world").replace("input", args[5]).send(sender);
             return false;
         }
 
         Location location = new Location(world, x, y, z);
 
-        NpcModifyEvent npcModifyEvent = new NpcModifyEvent(npc, NpcModifyEvent.NpcModification.LOCATION, location, receiver);
+        NpcModifyEvent npcModifyEvent = new NpcModifyEvent(npc, NpcModifyEvent.NpcModification.LOCATION, location, sender);
         npcModifyEvent.callEvent();
 
         if (!npcModifyEvent.isCancelled()) {
             npc.getData().setLocation(location);
             npc.updateForAll();
-            MessageHelper.success(receiver, lang.get("npc-command-teleport-success"));
+            translator.translate("command_invalid_world")
+                    .replace("npc", npc.getData().getName())
+                    .replace("x", COORDS_FORMAT.format(x))
+                    .replace("y", COORDS_FORMAT.format(x))
+                    .replace("z", COORDS_FORMAT.format(x))
+                    .replace("world", world.getName())
+                    .send(sender);
         } else {
-            MessageHelper.error(receiver, lang.get("npc-command-modification-cancelled"));
+            translator.translate("command_npc_modification_cancelled").send(sender);
         }
 
         return true;

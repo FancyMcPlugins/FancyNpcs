80903ac616813e40125d0c973389577037620022
Michał Czopek <44530932+Grabsky@users.noreply.github.com>
2024-05-20 18:34:57 +0200

    fix help pagination (again)

--- src/main/java/de/oliver/fancynpcs/commands/npc/HelpCMD.java
+++ src/main/java/de/oliver/fancynpcs/commands/npc/HelpCMD.java
@@ -6,6 +6,7 @@ import de.oliver.fancynpcs.FancyNpcs;
 import org.bukkit.command.CommandSender;
 import org.incendo.cloud.annotations.Argument;
 import org.incendo.cloud.annotations.Command;
+import org.incendo.cloud.annotations.Default;
 import org.incendo.cloud.annotations.Permission;
 import org.incendo.cloud.annotations.suggestion.Suggestions;
 import org.incendo.cloud.context.CommandContext;
@@ -15,7 +16,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 
 public enum HelpCMD {
     INSTANCE; // SINGLETON
@@ -26,28 +26,24 @@ public enum HelpCMD {
     @Permission("fancynpcs.command.npc")
     public void onHelp(
             final @NotNull CommandSender sender,
-            final @Nullable @Argument(suggestions = "HelpCMD/page") Integer page
+            final @Argument(suggestions = "HelpCMD/page") @Default("1") int page
     ) {
         // Getting the (full) help contents.
         final MultiMessage contents = (MultiMessage) translator.translate("npc_help_contents");
         // Calculating max page number.
-        final int maxPage = (int) Math.ceil(Math.max(1, contents.getRawMessages().size() - 1) / 6F);
+        final int maxPage = (int) Math.ceil(contents.getRawMessages().size() / 6F);
         // Getting the requested page. Defaults to 1 for invalid input and is capped by number of the last page.
-        final int finalPage = Math.min(page != null ? page : 1, maxPage);
-        // Getting help contents for requested page, or defaulting to 1.
-        final MultiMessage requestedContents = contents.page(finalPage, 6);
-        // Sending help header to the sender.
+        final int finalPage = Math.clamp(page, 1, maxPage);
+        // Sending help contents to the sender.
         translator.translate("npc_help_page_header").replace("page", String.valueOf(finalPage)).replace("max_page", String.valueOf(maxPage)).send(sender);
-        // Sending (requested) help contents to the sender.
-        requestedContents.send(sender);
-        // Sending help footer to the sender.
+        contents.page(finalPage, 6).send(sender);
         translator.translate("npc_help_page_footer").replace("page", String.valueOf(finalPage)).replace("max_page", String.valueOf(maxPage)).send(sender);
     }
 
     /* PARSERS AND SUGGESTIONS */
 
     @Suggestions("HelpCMD/page")
-    public List<String> suggestions(final CommandContext<CommandSender> context, final CommandInput input) {
+    public List<String> suggestPage(final CommandContext<CommandSender> context, final CommandInput input) {
         // Getting the (full) help contents.
         final MultiMessage contents = (MultiMessage) translator.translate("npc_help_contents");
         // Calculating max page number.

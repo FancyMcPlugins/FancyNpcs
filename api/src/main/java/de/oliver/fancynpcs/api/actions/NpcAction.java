1f8e89a6031eb57a7f1681f1580e7062dd0b0151
OliverSchlueter <OliverSchlueter@users.noreply.github.com>
2024-07-18 12:09:55 +0200

    lets go

---
+++ api/src/main/java/de/oliver/fancynpcs/api/actions/NpcAction.java
@@ -0,0 +1,21 @@
+package de.oliver.fancynpcs.api.actions;
+
+import de.oliver.fancynpcs.api.Npc;
+import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+public abstract class NpcAction {
+
+    private final String name;
+
+    public NpcAction(String name) {
+        this.name = name;
+    }
+
+    public abstract void execute(@NotNull Npc npc, @Nullable Player player, @Nullable String value);
+
+    public String getName() {
+        return name;
+    }
+}

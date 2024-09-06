fdea19769038bfc6adfefa04b2874c1be856c91c
OliverSchlueter <OliverSchlueter@users.noreply.github.com>
2024-09-06 20:40:34 +0200

    fix npc list footer when player-npcs fflag is enabled

--- src/main/java/de/oliver/fancynpcs/commands/npc/ListCMD.java
+++ src/main/java/de/oliver/fancynpcs/commands/npc/ListCMD.java
@@ -9,25 +9,24 @@ import org.bukkit.entity.Player;
 import org.incendo.cloud.annotations.Command;
 import org.incendo.cloud.annotations.Flag;
 import org.incendo.cloud.annotations.Permission;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import java.text.DecimalFormat;
 import java.util.Comparator;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Stream;
 
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
-
 public enum ListCMD {
     INSTANCE; // SINGLETON
-
-    private final Translator translator = FancyNpcs.getInstance().getTranslator();
 
     private static final DecimalFormat COORDS_FORMAT = new DecimalFormat("#.##");
 
     static {
         COORDS_FORMAT.setMinimumFractionDigits(2);
     }
+
+    private final Translator translator = FancyNpcs.getInstance().getTranslator();
 
     @Command("npc list")
     @Permission("fancynpcs.command.npc.list")
@@ -46,7 +45,8 @@ public enum ListCMD {
         // Sorting based on SortType choice. Defaults to SortType.NAME. There might be more sort types in the future which should be handled here accordingly.
         switch (sort != null ? sort : SortType.NAME) {
             case NAME -> stream = stream.sorted(Comparator.comparing(npc -> npc.getData().getName()));
-            case NAME_REVERSED -> stream = stream.sorted(Comparator.comparing(npc -> ((Npc) npc).getData().getName()).reversed()); // This needs a cast for some reason.
+            case NAME_REVERSED ->
+                    stream = stream.sorted(Comparator.comparing(npc -> ((Npc) npc).getData().getName()).reversed()); // This needs a cast for some reason.
         }
         translator.translate("npc_list_header").send(sender);
         // Using AtomicInteger counter because streams don't expose entry index.
@@ -62,11 +62,12 @@ public enum ListCMD {
                     .replace("world", npc.getData().getLocation().getWorld().getName())
                     .send(sender);
         });
-        final int totalCount = FancyNpcs.getInstance().getNpcManager().getAllNpcs().size();
+
+        final int totalCount = count.get(); // change this, once we have a page system
         translator.translate("npc_list_footer")
                 .replace("count", String.valueOf(count))
                 .replace("count_formatted", "· ".repeat(3 - String.valueOf(count).length()) + count)
-                .replace("total", String.valueOf(FancyNpcs.getInstance().getNpcManager().getAllNpcs().size()))
+                .replace("total", String.valueOf(totalCount))
                 .replace("total_formatted", "· ".repeat(3 - String.valueOf(totalCount).length()) + totalCount)
                 .send(sender);
     }

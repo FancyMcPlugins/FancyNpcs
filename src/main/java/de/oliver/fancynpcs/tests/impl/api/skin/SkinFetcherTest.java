c34e45da9bf687e51c61b0d409ecfcf45ab1c0fa
Oliver <oschi1303@gmail.com>
2024-11-21 13:31:13 +0100

    Add tests for file skins

--- src/main/java/de/oliver/fancynpcs/tests/impl/api/skin/SkinFetcherTest.java
+++ src/main/java/de/oliver/fancynpcs/tests/impl/api/skin/SkinFetcherTest.java
@@ -1,10 +1,13 @@
 package de.oliver.fancynpcs.tests.impl.api.skin;
 
+import de.oliver.fancynpcs.FancyNpcs;
 import de.oliver.fancynpcs.api.skins.SkinData;
 import de.oliver.fancynpcs.skins.SkinFetcherImpl;
 import de.oliver.fancynpcs.tests.annotations.FNBeforeEach;
 import de.oliver.fancynpcs.tests.annotations.FNTest;
 import org.bukkit.entity.Player;
+
+import java.io.File;
 
 public class SkinFetcherTest {
 
@@ -51,4 +54,30 @@ public class SkinFetcherTest {
         assert skin.textureSignature() != null && !skin.textureSignature().isEmpty();
     }
 
+    @FNTest(name = "Test fetch skin by file")
+    public void testSkinByFile(Player player) {
+        SkinData skin = skinFetcher.getByFile("plugins/FancyNpcs/testskin.png");
+        FancyNpcs.getInstance().getPlugin().saveResource("testskin.png", false);
+
+        assert skin != null;
+        assert skin.identifier().equals("src/test/resources/skin.png");
+        assert skin.type().equals(SkinData.SkinType.FILE);
+        assert skin.variant().equals(SkinData.SkinVariant.DEFAULT);
+        assert skin.textureValue() != null && !skin.textureValue().isEmpty();
+        assert skin.textureSignature() != null && !skin.textureSignature().isEmpty();
+
+        new File("plugins/FancyNpcs/testskin.png").delete();
+    }
+
+    @FNTest(name = "Test get skin")
+    public void testGetSkin(Player player) {
+        SkinData skin = skinFetcher.get("skinname", "value", "signature");
+
+        assert skin != null;
+        assert skin.identifier().equals("skinname");
+        assert skin.type().equals(SkinData.SkinType.VALUE_SIGNATURE);
+        assert skin.variant().equals(SkinData.SkinVariant.DEFAULT);
+        assert skin.textureValue().equals("value");
+        assert skin.textureSignature().equals("signature");
+    }
 }

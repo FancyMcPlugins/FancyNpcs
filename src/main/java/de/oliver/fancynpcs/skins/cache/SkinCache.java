3380b27ae9e7b2147b8b38ecf0bd5ea1e5b37275
Oliver <oschi1303@gmail.com>
2024-11-23 11:31:35 +0100

    caching

--- src/main/java/de/oliver/fancynpcs/skins/cache/SkinCache.java
+++ src/main/java/de/oliver/fancynpcs/skins/cache/SkinCache.java
@@ -1,10 +1,14 @@
 package de.oliver.fancynpcs.skins.cache;
 
+import de.oliver.fancynpcs.api.skins.SkinData;
+
 public interface SkinCache {
+
+    long CACHE_TIME = 1000 * 60 * 60 * 24 * 7; // 1 week
 
     SkinCacheData getSkin(String identifier);
 
-    void addSkin(SkinCacheData skin);
+    void addSkin(SkinData skin);
 
     void removeSkin(String identifier);
 

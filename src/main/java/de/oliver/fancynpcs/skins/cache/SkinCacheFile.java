082a0b876028c2d04d948e8a66d863ef25c288bd
Oliver <oschi1303@gmail.com>
2024-11-23 13:56:31 +0100

    Encode skin identifiers in Base64 for storage consistency

--- src/main/java/de/oliver/fancynpcs/skins/cache/SkinCacheFile.java
+++ src/main/java/de/oliver/fancynpcs/skins/cache/SkinCacheFile.java
@@ -5,6 +5,7 @@ import de.oliver.fancynpcs.FancyNpcs;
 import de.oliver.fancynpcs.api.skins.SkinData;
 
 import java.io.IOException;
+import java.util.Base64;
 
 public class SkinCacheFile implements SkinCache {
 
@@ -16,9 +17,11 @@ public class SkinCacheFile implements SkinCache {
 
     @Override
     public SkinCacheData getSkin(String identifier) {
+        String b64ID = Base64.getEncoder().encodeToString(identifier.getBytes());
+
         SkinCacheData skinCacheData = null;
         try {
-            skinCacheData = this.storage.get("skins/" + identifier, SkinCacheData.class);
+            skinCacheData = this.storage.get("skins/" + b64ID, SkinCacheData.class);
         } catch (IOException e) {
             FancyNpcs.getInstance().getFancyLogger().error("Failed to load skin cache");
             FancyNpcs.getInstance().getFancyLogger().error(e);
@@ -29,7 +32,7 @@ public class SkinCacheFile implements SkinCache {
         }
 
         if (skinCacheData.isExpired()) {
-            this.storage.delete("skins/" + identifier);
+            this.storage.delete("skins/" + b64ID);
             return null;
         }
 
@@ -41,7 +44,8 @@ public class SkinCacheFile implements SkinCache {
         SkinCacheData skinCacheData = new SkinCacheData(skin, System.currentTimeMillis(), CACHE_TIME);
 
         try {
-            this.storage.set("skins/" + skin.getIdentifier(), skinCacheData);
+            String b64ID = Base64.getEncoder().encodeToString(skin.getIdentifier().getBytes());
+            this.storage.set("skins/" + b64ID, skinCacheData);
         } catch (IOException e) {
             FancyNpcs.getInstance().getFancyLogger().error("Failed to save skin cache");
             FancyNpcs.getInstance().getFancyLogger().error(e);
@@ -50,6 +54,7 @@ public class SkinCacheFile implements SkinCache {
 
     @Override
     public void removeSkin(String identifier) {
-        this.storage.delete("skins/" + identifier);
+        String b64ID = Base64.getEncoder().encodeToString(identifier.getBytes());
+        this.storage.delete("skins/" + b64ID);
     }
 }

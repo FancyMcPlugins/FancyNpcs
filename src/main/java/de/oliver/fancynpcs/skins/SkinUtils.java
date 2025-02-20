ecabd9a0beca8d9ad8f9e1ed1f5353125b5d46b3
Oliver <oschi1303@gmail.com>
2025-02-20 17:39:13 +0100

    remove uneeded stuff

--- src/main/java/de/oliver/fancynpcs/skins/SkinUtils.java
+++ src/main/java/de/oliver/fancynpcs/skins/SkinUtils.java
@@ -1,9 +1,5 @@
 package de.oliver.fancynpcs.skins;
 
-import de.oliver.fancynpcs.FancyNpcs;
-import de.oliver.fancynpcs.api.Npc;
-import de.oliver.fancynpcs.api.skins.SkinData;
-
 public class SkinUtils {
 
     public static boolean isPlaceholder(String identifier) {
@@ -20,41 +16,5 @@ public class SkinUtils {
 
     public static boolean isFile(String identifier) {
         return identifier.endsWith(".png") || identifier.endsWith(".jpg") || identifier.endsWith(".jpeg");
-    }
-
-    public static void applySkinLater(String npcID, String skinID, SkinData.SkinVariant variant, Runnable successCallback, Runnable errorCallback) {
-        MineSkinQueue.get().add(() -> {
-            FancyNpcs.getInstance().getFancyLogger().debug("Loading skin for npc '" + npcID + "'. Skin: " + skinID + ", Variant: " + variant);
-
-            SkinData skin = FancyNpcs.getInstance().getSkinManagerImpl().getByIdentifier(skinID, variant);
-            if (skin == null) {
-                FancyNpcs.getInstance().getFancyLogger().error("Could not fetch skin for npc '" + npcID + "'");
-                errorCallback.run();
-                return;
-            }
-
-            Npc npc = FancyNpcs.getInstance().getNpcManager().getNpcById(npcID);
-            if (npc == null) {
-                FancyNpcs.getInstance().getFancyLogger().error("Could not find npc '" + npcID + "'");
-                errorCallback.run();
-                return;
-            }
-
-            npc.getData().setMirrorSkin(false);
-            npc.getData().setSkin(skin);
-            npc.removeForAll();
-            npc.create();
-            npc.spawnForAll();
-
-            FancyNpcs.getInstance().getFancyLogger().debug("Successfully applied skin for npc '" + npcID + "'");
-            successCallback.run();
-        });
-        System.out.println("Added skin to queue");
-    }
-
-    public static void applySkinLater(String npcID, String skinID, SkinData.SkinVariant variant) {
-        applySkinLater(npcID, skinID, variant, () -> {
-        }, () -> {
-        });
     }
 }

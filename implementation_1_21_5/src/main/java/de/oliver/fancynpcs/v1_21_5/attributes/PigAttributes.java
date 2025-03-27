2d35df85764c2391d00118e808662fd2b0d1d1ce
Oliver <oschi1303@gmail.com>
2025-03-27 19:16:51 +0100

    Add pig variant attribute

--- implementation_1_21_5/src/main/java/de/oliver/fancynpcs/v1_21_5/attributes/PigAttributes.java
+++ implementation_1_21_5/src/main/java/de/oliver/fancynpcs/v1_21_5/attributes/PigAttributes.java
@@ -3,8 +3,15 @@ package de.oliver.fancynpcs.v1_21_5.attributes;
 import de.oliver.fancynpcs.api.Npc;
 import de.oliver.fancynpcs.api.NpcAttribute;
 import de.oliver.fancynpcs.v1_21_5.ReflectionHelper;
+import net.minecraft.core.Holder;
+import net.minecraft.core.HolderLookup;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.data.registries.VanillaRegistries;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.animal.Pig;
+import net.minecraft.world.entity.animal.PigVariant;
 import net.minecraft.world.item.Items;
 import org.bukkit.entity.EntityType;
 
@@ -17,6 +24,16 @@ public class PigAttributes {
         List<NpcAttribute> attributes = new ArrayList<>();
 
         attributes.add(new NpcAttribute(
+                "variant",
+                getPigVariantRegistry()
+                        .listElementIds()
+                        .map(id -> id.location().getPath())
+                        .toList(),
+                List.of(EntityType.CAT),
+                PigAttributes::setVariant
+        ));
+
+        attributes.add(new NpcAttribute(
                 "has_saddle",
                 List.of("true", "false"),
                 List.of(EntityType.PIG),
@@ -24,6 +41,19 @@ public class PigAttributes {
         ));
 
         return attributes;
+    }
+
+    private static void setVariant(Npc npc, String value) {
+        final Pig pig = ReflectionHelper.getEntity(npc);
+
+        Holder<PigVariant> variant = getPigVariantRegistry()
+                .get(ResourceKey.create(
+                        Registries.PIG_VARIANT,
+                        ResourceLocation.withDefaultNamespace(value.toLowerCase())
+                ))
+                .orElseThrow();
+
+        pig.setVariant(variant);
     }
 
     private static void setHasSaddle(Npc npc, String value) {
@@ -34,6 +64,13 @@ public class PigAttributes {
         if (hasSaddle) {
             pig.setItemSlot(EquipmentSlot.SADDLE, Items.SADDLE.getDefaultInstance());
         }
+    }
+
+    private static HolderLookup.RegistryLookup<PigVariant> getPigVariantRegistry() {
+        return VanillaRegistries
+                .createLookup()
+                .lookup(Registries.PIG_VARIANT)
+                .orElseThrow();
     }
 
 }
